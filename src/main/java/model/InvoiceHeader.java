fe93035a3e665be3f53b9b95c2fbd880f37774bb
Ahmed Magdy <69248765+AMF777@users.noreply.github.com>
2022-10-21 19:39:25 +0200

    Add files via upload

---
+++ src/main/java/model/InvoiceHeader.java
@@ -0,0 +1,78 @@
+/*
+Author Ahmed Magdy
+File Name:Invoice Header this java class is to handle writing and reading from CSV files
+*/
+package model;
+
+import java.util.ArrayList;
+
+
+public class InvoiceHeader {
+    //attributes
+    private int number;
+    private String date;
+    private String custmor;
+    private ArrayList<InvoiceLine> lines;
+    private double invoiceTotal;
+
+    //default Constructor
+    public InvoiceHeader() {
+    }
+    //implement attributes setters abd gettters
+public double getInvoiceTotal(){
+    double invoiceTotal = 0.0;
+    for (InvoiceLine line : getLines()){
+        invoiceTotal =invoiceTotal+ line.getlinesTotal();
+    }
+    return invoiceTotal;   
+}
+    //constructor
+    public InvoiceHeader(int number, String date, String custmor) {
+        this.number = number;
+        this.date = date;
+        this.custmor = custmor;
+    }
+
+    public ArrayList<InvoiceLine> getLines() {
+        if (lines == null){
+            lines = new ArrayList<>();
+        }
+        return lines;
+    }
+
+    public String getCustmor() {
+        return custmor;
+    }
+
+    public void setCustmor(String custmor) {
+        this.custmor = custmor;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "header{" + "number=" + number + ", date=" + date + ", custmor=" + custmor + '}';
+    }
+   public String returnAsCSV(){
+       return number+","+date+","+custmor;
+   } 
+
+    void add(InvoiceLine line) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
